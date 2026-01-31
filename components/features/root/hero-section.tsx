aaf243a06a1ae24ebea326c7ce8c59e98c6b47d1
Naim Ahmad <naimahmad201103@proton.me>
2026-02-01 01:34:34 +0600

    changed text size

--- components/features/root/hero-section.tsx
+++ components/features/root/hero-section.tsx
@@ -42,34 +42,8 @@ export function HeroSection() {
                   Launch Platform
                 </Link>
 
-                <button className="flex h-9 px-4 py-3 justify-center items-center gap-[11.916px] border-[1px]  rounded-[4.766px] border-spout-light-gray text-primary font-dm-sans !text-black font-[16px] text-lg sm:text-xl transition-all group">
+                <button className="flex h-9 px-4 py-3 justify-center items-center gap-[11.916px] border-[1px]  rounded-[4.766px] border-spout-light-gray text-primary font-dm-sans !text-black  text-[16px] transition-all group">
                   Try Demo
-                  {/* <div className="bg-spout-primary w-[35px] h-[32px] rounded-[3px] text-white flex items-center justify-center transition-transform group-hover:translate-x-2">
-                    <svg
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M14.43 5.92999L20.5 12L14.43 18.07"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeMiterlimit="10"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                      <path
-                        d="M3.5 12H20.33"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeMiterlimit="10"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                  </div> */}
                 </button>
               </div>
 
